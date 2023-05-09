384378679671adb4241bacf675172ab8ede2d75e
Chaeyoung Lim <rnldyal1027@naver.com>
2023-05-09 10:51:11 +0900

    기존 스타일대로 인스턴스 생성, 초기화

--- InstanceVariablePrac/Program.cs
+++ InstanceVariablePrac/Program.cs
@@ -9,7 +9,10 @@ namespace InstanceVariablePrac
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {   //자바 스타일
+            Product product1 = new Product();
+            product1.name = "감자";
+            product1.price = 2000;
         }
     }
 }
